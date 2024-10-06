28f928c3e771f33e2454790a8b8f59fecb636bda
EalZz <114982277+EalZz@users.noreply.github.com>
2024-10-06 15:26:14 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    string str;
+    //int n;
+    cin >> str;
+    
+    cout << str.size();
+    
+    return 0;
+}
