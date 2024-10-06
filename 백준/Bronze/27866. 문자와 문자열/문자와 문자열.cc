f7e0c5fa29a208a41fb851a49e0ef747610eeaa5
EalZz <114982277+EalZz@users.noreply.github.com>
2024-10-06 15:25:58 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    string str;
+    int n;
+    cin >> str >> n;
+    
+    cout << str[n - 1];
+    
+    return 0;
+}
