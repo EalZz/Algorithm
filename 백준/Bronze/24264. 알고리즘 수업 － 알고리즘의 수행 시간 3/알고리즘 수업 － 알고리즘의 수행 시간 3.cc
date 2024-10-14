a728e17186b6f8b7c2cb94d6421bd0e7fd1f1321
EalZz <114982277+EalZz@users.noreply.github.com>
2024-10-14 19:02:01 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <numeric>
+#include <climits>
+using namespace std;
+
+int main() {
+	unsigned long long n = 0;
+	cin >> n;
+	cout << n*n << endl << 2;
+
+	return 0;
+}
