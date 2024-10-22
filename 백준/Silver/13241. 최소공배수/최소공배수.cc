60c969397ba2f2357cbeddff25df8627621391c2
EalZz <114982277+EalZz@users.noreply.github.com>
2024-10-22 20:46:17 +0900

    [Silver V] Title: 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <set>
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    long long int a = 0, b = 0, c = 0;
+    cin >> a >> b;
+    long long int mul = a * b;
+
+    while (b) {
+        c = a % b;
+        a = b;
+        b = c;
+    }
+    cout << mul / a << "\n";
+    
+
+    return 0;
+}
