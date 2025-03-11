b754cfc8438dbe4df3c4e3e3e853ed43d4aa3b2d
EalZz <114982277+EalZz@users.noreply.github.com>
2025-03-11 16:42:52 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.10 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+string solution(int n) {
+    string s;
+    for(int i = 1; i <= n; i++){
+        s += (i % 2 == 0) ? "박" : "수";
+    }
+    return s;
+}
