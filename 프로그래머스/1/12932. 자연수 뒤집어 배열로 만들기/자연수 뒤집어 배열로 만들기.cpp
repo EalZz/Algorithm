930e71cb1d9be83c21bb353bbe7a934cd0afe607
EalZz <114982277+EalZz@users.noreply.github.com>
2025-03-12 16:32:47 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+vector<int> solution(long long n) {
+    vector<int> answer;
+    string s = to_string(n);
+    for(int i = s.size() - 1; i >= 0; i--) answer.push_back(s[i] - '0');
+    return answer;
+}
