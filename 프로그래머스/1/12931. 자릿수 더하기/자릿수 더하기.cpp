e78241b0435ad4d35ae7171635807de64b48ff99
EalZz <114982277+EalZz@users.noreply.github.com>
2025-03-12 16:21:56 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cpp
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cpp
@@ -1,18 +1,13 @@
 #include <iostream>
-#include <cmath>
-
 using namespace std;
-int solution(int n)
-{
-    int answer = 0, cnt = 1;
-    
-    for(int i = n; i > 0; i /= 10) cnt++;
-    
-    while(cnt--){
-        int num = n / pow(10, cnt - 1);
-        answer += num;
-        n -=  num * pow(10, cnt - 1);
+
+int solution(int n) {
+    int answer = 0;
+
+    while (n > 0) {
+        answer += n % 10;  
+        n /= 10;           
     }
-        
+
     return answer;
 }
