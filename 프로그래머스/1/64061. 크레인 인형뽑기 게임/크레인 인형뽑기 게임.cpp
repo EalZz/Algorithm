56bdbf12ed3b69b336eab331957b4ee09ca918a6
EalZz <114982277+EalZz@users.noreply.github.com>
2025-03-21 17:48:10 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.04 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
@@ -8,14 +8,16 @@ int solution(vector<vector<int>> board, vector<int> moves) {
     int cnt = 0;
     for(int i = 0; i < moves.size(); i++) {
         for(int j = 0; j < board.size(); j++){
-            if(!board[j][moves[i] - 1]) continue;
-            if(basket.size() != 0 && basket.back() == board[j][moves[i] - 1]) {
-                basket.pop_back();
-                cnt += 2;
+            if(!board[j][moves[i] - 1]) continue; // 빈곳이면 다음 칸
+
+            if(basket.size() != 0 && basket.back() == board[j][moves[i] - 1]) { //같은 것끼리 만났을 때
+                basket.pop_back(); //터지니 위에있는 것 pop, 따로 추가 안함
+                cnt += 2; 
             }
-            else basket.push_back(board[j][moves[i] - 1]);
-            board[j][moves[i] - 1] = 0;
-            break;
+            else basket.push_back(board[j][moves[i] - 1]); //바구니에 쌓일 때
+            
+            board[j][moves[i] - 1] = 0; //빈공간으로 만들어줌
+            break; //한번 진행 했으니 다음 무브로 이동
         }
     }
     return cnt;
