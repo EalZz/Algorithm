18b609e8d8db2c2c90aa3c6f02a3f58a08be4645
EalZz <114982277+EalZz@users.noreply.github.com>
2025-04-01 10:27:03 +0900

    [level 1] Title: 삼총사, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.cpp
+++ 프로그래머스/1/131705. 삼총사/삼총사.cpp
@@ -7,10 +7,8 @@ int solution(vector<int> number) {
     int answer = 0;
     for(int i = 0; i < number.size() - 2; i++) {
         for(int j = i + 1; j < number.size() - 1; j++) {
-            for(int k = j + 1; k < number.size(); k++) {
-                int sum = number[i] + number[j] + number[k];
-                if(!sum) answer++;
-            }
+            for(int k = j + 1; k < number.size(); k++) 
+                if(!(number[i] + number[j] + number[k])) answer++;
         }
     }
     
