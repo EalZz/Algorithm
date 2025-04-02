ace3dbbe9009a317c8ac0c3269f830ac5e7aea65
EalZz <114982277+EalZz@users.noreply.github.com>
2025-04-02 10:58:39 +0900

    [level 1] Title: 옹알이 (2), Time: 0.22 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+int solution(vector<string> babbling) {
+    int answer = 0;
+
+    vector<string> ong {"aya", "ye", "woo", "ma"};
+    for(auto bab : babbling){
+        bool possible = true;
+
+        for(int i = 0; i < ong.size(); i++){
+            auto it = bab.find(ong[i]);
+            while ((it = bab.find(ong[i], it)) != string::npos) {
+                bab.erase(it, ong[i].length());
+                bab.insert(it, 1, '0' + i); 
+                it++; 
+            }
+        }
+
+        cout << bab << endl;
+        
+        if (!bab.empty() && (bab[0] < '0' || bab[0] > '9')) possible = false;
+
+        for (size_t i = 1; i < bab.size(); i++) {
+            if (bab[i] < '0' || bab[i] > '9' || bab[i] == bab[i - 1]) {
+                possible = false;
+                break;
+            }
+        }
+        
+        if(possible) answer++;
+    }
+    return answer;
+}
