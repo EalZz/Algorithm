19faacede5c6cb611f733dea12ffc402b1f3ea46
EalZz <114982277+EalZz@users.noreply.github.com>
2025-04-04 11:11:54 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.68 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cpp
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cpp
@@ -5,26 +5,15 @@
 using namespace std;
 
 vector<int> solution(int k, vector<int> score) {
-    vector<int> answer;
-    vector<int> temp;
-    
-    answer.push_back(score[0]);
-    temp.push_back(score[0]);
-    if(score.size() > k) {
-        for(int i = 1; i < k; i++) {
-            answer.push_back(score[i] < answer[i - 1] ? score[i] : answer[i - 1]);
-            temp.push_back(score[i]);
-        }
-        for(int i = k; i < score.size(); i++) {
-            temp.push_back(score[i]);
-            sort(temp.begin(), temp.end(), [](int x, int y) {return x > y;});
-            answer.push_back(temp[k - 1]);
-        }
+    vector<int> answer, tmp;
+
+    for(auto s : score){
+        tmp.push_back(s);
+        sort(tmp.begin(), tmp.end(), greater<int>());
+        
+        if(tmp.size() >= k) tmp.erase(tmp.begin() + k, tmp.end());
+        answer.push_back(tmp.back());
     }
-    else {
-        for(int i = 1; i < score.size(); i++)
-            answer.push_back(score[i] < answer[i - 1] ? score[i] : answer[i - 1]);
-    }
-    
+
     return answer;
 }
