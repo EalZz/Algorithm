69c7b4a47c59dd04ba6a21fffc42b6a0bc2026ca
EalZz <114982277+EalZz@users.noreply.github.com>
2025-04-04 15:22:43 +0900

    [level 1] Title: 콜라 문제, Time: 0.03 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
@@ -8,8 +8,8 @@ int solution(int a, int b, int n) {
     int answer = 0;
     
     while (n >= a) {
-        answer += n / a * b;     
-        n = n / a * b + n % a;       
+        answer += n / a * b;   //교환한 수
+        n = n / a * b + n % a; //교환한 수 + 교환에 사용하지 않은 병 수
     }
     return answer;
 }
