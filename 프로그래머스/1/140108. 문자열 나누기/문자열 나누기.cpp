29e6624b3facf93e07481f80807696f21465a4a8
EalZz <114982277+EalZz@users.noreply.github.com>
2025-04-04 12:26:10 +0900

    [level 1] Title: 문자열 나누기, Time: 0.06 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.cpp
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.cpp
@@ -6,27 +6,32 @@ using namespace std;
 int solution(string s) {
     int answer = 0;
     bool change = false;
-    char tmp = s[0];
-    int cntx = 1, cnta = 0;
+    char tmp = s[0]; 
+    int cntx = 1, cnta = 0; //첫문자와 카운트 +1
     
-    if (s.size() == 1) return 1;
+    //문자열 길이가 1이면 1 반환
+    if (s.size() == 1) return 1; 
     
     for(int i = 1; i < s.size(); i++) {
+        //문자열을 새로 끊을 때
         if(change){
             tmp = s[i];
             cntx = 0;
             cnta = 0;
             change = false;
         }
-            
+        
+        //각각 카운트에 추가
         if(s[i] == tmp) cntx++;
         else cnta++;
         
+        //카운트가 같아지면 answer++ 후 문자열 끊기
         if(cntx == cnta) {
             answer++;
             change = true;
             continue;
         }
+        //마지막까지 카운트가 안맞으면 1추가
         if(i == s.size() - 1) answer++;
     }
     return answer;
