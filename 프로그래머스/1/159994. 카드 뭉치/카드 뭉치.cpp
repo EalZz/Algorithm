b001d1dc3bb981e969fb735b271f1fc37575e9ac
EalZz <114982277+EalZz@users.noreply.github.com>
2025-04-09 15:17:31 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
@@ -5,8 +5,9 @@ using namespace std;
 
 string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
     int idx1 = 0, idx2 = 0;
-    for (string& word : goal) {
-        //차례대로 카드 찾기
+    for (string word : goal) {
+        //사용해야 하는 카드 중 나와야하는 카드가 있는지 체크
+        //있으면 인덱스 증가 후 다음 단어 탐색, 둘 다 없으면 No반환
         if (idx1 < cards1.size() && cards1[idx1] == word) idx1++;
         else if (idx2 < cards2.size() && cards2[idx2] == word) idx2++; 
         else return "No"; 
