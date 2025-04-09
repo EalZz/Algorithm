1fcd9ab8443ab2a4d6ebef2cbf01d7d504388623
EalZz <114982277+EalZz@users.noreply.github.com>
2025-04-09 15:45:16 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.cpp
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.cpp
@@ -3,6 +3,33 @@
 
 using namespace std;
 
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<string>> board, int h, int w) {
+    int answer = 0;
+
+    int dh[] = {-1, 1, 0, 0};
+    int dw[] = {0, 0, -1, 1};
+
+    for (int i = 0; i < 4; i++) {
+        int nh = h + dh[i];
+        int nw = w + dw[i];
+
+        // 경계 체크
+        if (nh >= 0 && nh < board.size() && nw >= 0 && nw < board[0].size()) {
+            if (board[h][w] == board[nh][nw]) {
+                answer++;
+            }
+        }
+    }
+
+    return answer;
+}
+
+/*
 int solution(vector<vector<string>> board, int h, int w) {
     int answer = 0;
     if(h == 0) {
@@ -56,3 +83,4 @@ int solution(vector<vector<string>> board, int h, int w) {
 
     return answer;
 }
+*/
