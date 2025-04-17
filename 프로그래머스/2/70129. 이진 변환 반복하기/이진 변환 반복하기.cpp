d6250f0eda26f3079dbf0315d5844cc6c447e5d7
EalZz <114982277+EalZz@users.noreply.github.com>
2025-04-17 13:24:24 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 0.20 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(string s) {
+    vector<int> answer(2);
+
+    while (s != "1") {
+        int cnt = count(s.begin(), s.end(), '0'); 
+        answer[1] += cnt;
+
+        int len = s.size() - cnt; // 1의 개수
+        s = "";
+
+        while (len > 0) {
+            s = char(len % 2 + '0') + s;
+            len /= 2;
+        }
+
+        answer[0]++;
+    }
+
+    return answer;
+}
