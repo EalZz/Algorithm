0309e62f1cad8b246724086117a09b80c5de80bc
EalZz <114982277+EalZz@users.noreply.github.com>
2025-07-17 20:44:52 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N, K;
+    cin >> N >> K;
+
+    queue<int> q;
+    for (int i = 1; i <= N; i++) q.push(i);
+
+    cout << "<";
+    while (!q.empty()) {
+        for (int i = 0; i < K - 1; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+        cout << q.front();
+        q.pop();
+
+        if (!q.empty()) cout << ", ";
+    }
+    cout << ">";
+    return 0;
+}
