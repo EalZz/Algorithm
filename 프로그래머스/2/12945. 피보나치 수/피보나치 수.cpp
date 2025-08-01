0a96f843b3ed05279142f94a3421e762151f3a0f
EalZz <114982277+EalZz@users.noreply.github.com>
2025-08-02 04:03:25 +0900

    [level 2] Title: 피보나치 수, Time: 3.23 ms, Memory: 7.02 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -3,12 +3,21 @@
 
 using namespace std;
 
+int Fibonacci(int n);
+int memoization[100000];
+
 int solution(int n) {
-    int answer = 1, tmp1 = 1, tmp2 = 0;
-    while(--n) {
-        answer = (tmp1 + tmp2) % 1234567;
-        tmp2 = tmp1;
-        tmp1 = answer;
+    return Fibonacci(n) ;
+}
+
+int Fibonacci(int n) {
+    if(n == 0) return memoization[n];
+    else if(n == 1) {
+        if(!memoization[n]) memoization[n] = 1;
+        return memoization[n];
     }
-    return answer;
+    
+    //모듈러 산술 (오버플로우 방지를 위해 %1234567을 매번 적용)
+    if(!memoization[n]) memoization[n] = (Fibonacci(n - 1) + Fibonacci(n - 2)) % 1234567;
+    return memoization[n];
 }
