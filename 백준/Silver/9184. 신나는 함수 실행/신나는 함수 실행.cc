54d14964672eb84d7e8bf4b108b57a3619570236
EalZz <114982277+EalZz@users.noreply.github.com>
2025-08-04 17:43:56 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 8 ms, Memory: 2056 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <memory>
+
+using namespace std;
+//int qmatrix(vector<vector<int>>& matrix, int row, int col, int T);
+int w(int a, int b, int c);
+int memo[21][21][21];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    memset(memo, -1, sizeof(memo));
+    //int N = 0, K = 0; 
+    //cin >> N >> K;
+    //int T = 0; cin >> T;
+    int a = 0, b = 0, c = 0;
+    
+    while (1) {
+        cin >> a >> b >> c;
+        if (a == -1 && b == -1 && c == -1) break;
+        cout << "w(" << a << ", " << b << ", " << c << ") = " << w(a, b, c) << '\n';
+    }
+
+    return 0;
+}
+
+int w(int a, int b, int c) {
+    if (a <= 0 || b <= 0 || c <= 0) return 1;
+    if (a > 20 || b > 20 || c > 20) return w(20, 20, 20);
+
+    if (memo[a][b][c] != -1)  return memo[a][b][c];
+
+    int tmp = 0;
+
+    if (a < b && b < c) tmp = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+    else tmp = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+
+    memo[a][b][c] = tmp;
+    return tmp;
+}
+
+int qmatrix(vector<vector<int>>& matrix, int row, int col, int T) {
+    return 0;
+}
