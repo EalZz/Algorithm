1162e252b10cf0ea7b57eac3bbb3c84c0bd0397e
EalZz <114982277+EalZz@users.noreply.github.com>
2025-08-08 18:32:39 +0900

    [Silver II] Title: 부분수열의 합, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -13,32 +13,35 @@
 
 using namespace std;
 int cnt = 0;
-void subset(vector<int> v, int K, int idx, int sum);
+
+void subset(vector<int>& v, int target, int sum, int idx);
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    int N = 0, K = 0; 
-    cin >> N >> K;
-    //int T = 0; cin >> T;
-    vector<int> v(N);
-    for (int i = 0; i < N; i++) cin >> v[i];
+    int T = 0; cin >> T;
+    int N = 0; cin >> N;
+    //int a = 0, b = 0, c = 0;
 
-    subset(v, K, 0, 0);
+    vector<int> v(T);
+    for (int i = 0; i < T; i++) cin >> v[i];
+
+    subset(v, N, 0, 0);
     cout << cnt;
 
     return 0;
 }
 
-void subset(vector<int> v, int K, int idx, int sum) {
-    if (idx >= v.size()) return;
+void subset(vector<int>& v, int target, int sum, int idx) {
+    if (idx == v.size()) return;
 
-    subset(v, K, idx + 1, sum);
+    subset(v, target, sum, idx + 1);
 
     sum += v[idx];
-    subset(v, K, idx + 1, sum);
-    if (sum == K) cnt++;
+    subset(v, target, sum, idx + 1);
+
+    if (sum == target) cnt++;
 
     return;
 }
