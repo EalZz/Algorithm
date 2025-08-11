27d086520e49d2ea4d2c1612abd769b70a596bbe
EalZz <114982277+EalZz@users.noreply.github.com>
2025-08-12 00:26:27 +0900

    [level 3] Title: 네트워크, Time: 0.05 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -3,24 +3,25 @@
 
 using namespace std;
 
-void dfs(int node, vector<vector<int>>& computers, vector<bool>& isVisited);
+void dfs(int n, vector<vector<int>>& computers, vector<bool>& isVisited);
 
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
     vector<bool> isVisited(n, false);
     
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < computers.size(); i++){
         if(!isVisited[i]) {
             answer++;
             dfs(i, computers, isVisited);
         }
     }
+    
     return answer;
 }
 
 void dfs(int node, vector<vector<int>>& computers, vector<bool>& isVisited) {
     for(int i = 0; i < computers.size(); i++) {
-        if(!isVisited[i] && i != node && computers[node][i] == 1) {
+        if(i != node && !isVisited[i] && computers[node][i] == 1) {
             isVisited[i] = true;
             dfs(i, computers, isVisited);
         }
