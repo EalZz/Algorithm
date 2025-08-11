74f40367a1b143e2646739e4412c98b46139ca03
EalZz <114982277+EalZz@users.noreply.github.com>
2025-08-11 18:15:18 +0900

    [level 3] Title: 여행경로, Time: 0.03 ms, Memory: 4.06 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+bool dfs(string airport, vector<vector<string>>& tickets, vector<string>& answer, vector<bool>& isVisited);
+
+vector<string> solution(vector<vector<string>> tickets) {
+    vector<string> answer;
+    vector<bool> isVisited(tickets.size(), false);
+    answer.push_back("ICN");
+    
+    sort(tickets.begin(), tickets.end(), [](vector<string> a, vector<string> b){
+        if(a[0] != b[0]) return a[0] < b[0];
+        return a[1] < b[1];
+    });
+    
+    dfs("ICN", tickets, answer, isVisited);
+    
+    return answer;
+}
+
+bool dfs(string airport, vector<vector<string>>& tickets, vector<string>& answer, vector<bool>& isVisited) {
+    if(answer.size() == tickets.size() + 1) return true;
+    
+    for(int i = 0; i < tickets.size(); i++) {
+        if(airport == tickets[i][0] && !isVisited[i]) {
+            isVisited[i] = true;
+            answer.push_back(tickets[i][1]);
+            
+            if(dfs(tickets[i][1], tickets, answer, isVisited)) return true;
+            
+            answer.pop_back();
+            isVisited[i] = false;
+        }
+    }
+    return false;
+}
