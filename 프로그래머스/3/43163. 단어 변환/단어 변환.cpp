0550b6bb0d5adb77905f684320d93f45679d833a
EalZz <114982277+EalZz@users.noreply.github.com>
2025-08-24 19:23:02 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -3,32 +3,30 @@
 #include <queue>
 using namespace std;
 int solution(string begin, string target, vector<string> words) {
-    int answer = 0;
     queue<pair<string, int>> bfs;
     vector<bool> isVisited(words.size(), false);
     bfs.push({begin, 0});
     
     while(!bfs.empty()) {
-        string usingword = bfs.front().first;
+        string usingWard = bfs.front().first;
         int currentLevel = bfs.front().second;
         bfs.pop();
         
-        if(usingword == target) return currentLevel;
+        if(usingWard == target) return currentLevel;
         
         for(int i = 0; i < words.size(); i++) {
-            int cnt = 0; 
+            int cnt = 0;
             if(!isVisited[i]) {
                 for(int j = 0; j < words[i].size(); j++) {
-                    if(usingword[j] != words[i][j]) cnt++;
+                    if(usingWard[j] != words[i][j]) cnt++;
                     if(cnt > 1) break;
                 }
-                if(cnt == 1) {
-                    bfs.push({words[i], currentLevel + 1});
-                    isVisited[i] = true;
-                }
+            }
+            if(cnt == 1) {
+                isVisited[i] = true;
+                bfs.push({words[i], currentLevel + 1});
             }
         }
     }
-    
     return 0;
 }
