369308627a560f46fe4f0acfd7feccd2a5c4024d
EalZz <114982277+EalZz@users.noreply.github.com>
2025-09-08 13:18:36 +0900

    [Gold IV] Title: N-Queen, Time: 2052 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -15,51 +15,51 @@ using namespace std;
 int cnt = 0, cnt1 = 0;
 int memo[21][21][21];
 //int memo[10000001];
+void nQueen(int T, int row, vector<bool>& colA, vector<bool>& diag1, vector<bool>& diag2);
 
-void n_queen(int T, int row, int col, vector<bool>& colA, vector<bool>& diag1, vector<bool>& diag2);
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    memset(memo, -1, sizeof(memo));
+    //memset(memo, -1, sizeof(memo));
     int T = 0; cin >> T;
     //int N = 0; cin >> N;
     //int a = 0, b = 0, c = 0; cin >> a >> b >> c;
     //vector<int> v;
     //vector<int> tmp;
-    //vector<vector<int>> v(T, vector<int>(T));
+    vector<vector<int>> v(T, vector<int>(T));
     //vector<vector<int>> matrix(b, vector<int>(a, 0));
     //vector<vector<bool>> isVisited(T, vector<bool>(T, false));
-
+    
     vector<bool> colA(T, false);
-    vector<bool> diag1(T * 2 - 1, false);
-    vector<bool> diag2(T * 2 - 1, false);
+    vector<bool> diag1(2 * T - 1, false);
+    vector<bool> diag2(2 * T - 1, false);
 
-    for (int col = 0; col < T; col++) n_queen(T, 0, col, colA, diag1, diag2);
+    nQueen(T, 0, colA, diag1, diag2);
     cout << cnt;
 
     return 0;
 }
 
-void n_queen(int T, int row, int col, vector<bool>& colA, vector<bool>& diag1, vector<bool>& diag2) {
-    if (row == T - 1) {
+void nQueen(int T, int row, vector<bool>& colA, vector<bool>& diag1, vector<bool>& diag2) {
+    if (row == T) {
         cnt++;
         return;
     }
 
-    colA[col] = true;
-    diag1[(row - col) + T - 1] = true;
-    diag2[row + col] = true;
+    for (int col = 0; col < T; col++) {
+        if (!colA[col] && !diag1[row - col + T - 1] && !diag2[row + col]) {
+            colA[col] = true;
+            diag1[row - col +T - 1] = true;
+            diag2[col + row] = true;
 
-    for (int ncol = 0; ncol < T; ncol++) {
-        if (!colA[ncol] && !diag1[(row + 1 - ncol) + (T - 1)] && !diag2[row + 1 + ncol]) //다음 행으로 이동(row + 1)
-            n_queen(T, row + 1, ncol, colA, diag1, diag2);
+            nQueen(T, row + 1, colA, diag1, diag2);
+
+            colA[col] = false;
+            diag1[row - col + T -1] = false;
+            diag2[col + row] = false;
+        }
     }
-
-    colA[col] = false;
-    diag1[(row - col) + T - 1] = false;
-    diag2[row + col] = false;
-
     return;
 }
