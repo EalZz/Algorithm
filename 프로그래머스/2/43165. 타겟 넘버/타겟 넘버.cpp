b49a0944a89d1dc8e9dd7dd93a2b0a0b73d860d7
EalZz <114982277+EalZz@users.noreply.github.com>
2025-09-15 00:27:22 +0900

    [level 2] Title: 타겟 넘버, Time: 68.27 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -3,21 +3,21 @@
 
 using namespace std;
 int cnt = 0;
-void dfs(int sum, int idx, vector<int> numbers, int target);
+void dfs(int idx, int sum, int target, vector<int> numbers);
 
 int solution(vector<int> numbers, int target) {
-    dfs(0, 0, numbers, target);
+    dfs(0, 0, target, numbers);
     return cnt;
 }
 
-void dfs(int sum, int idx, vector<int> numbers, int target) {
+void dfs(int idx, int sum, int target, vector<int> numbers) {
     if(idx == numbers.size()) {
         if(sum == target) cnt++;
         return;
     }
     
-    dfs(sum + numbers[idx], idx + 1, numbers, target);
-    dfs(sum - numbers[idx], idx + 1, numbers, target);
+    dfs(idx + 1, sum + numbers[idx], target, numbers);
+    dfs(idx + 1, sum - numbers[idx], target, numbers);
     
     return;
 }
