bbddce6ec4b8e9a37d691643236de08ae8918ee4
EalZz <114982277+EalZz@users.noreply.github.com>
2025-09-17 13:15:01 +0900

    [D3] Title: 평행사변형, Time: 8 ms, Memory: 5,868 KB -BaekjoonHub

---
+++ SWEA/D3/15941. 평행사변형/평행사변형.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <memory>
+
+using namespace std;
+int cnt = 0, cnt1 = 0;
+int memo[21][21][21];
+//int memo[10000001];
+string change;
+
+void dfs(string N, int M, int count, set<pair<string, int>>& pruning);
+
+int main() {
+	int T = 0; cin >> T;
+
+	for (int t = 1; t <= T; t++) {
+		int a = 0; cin >> a;
+
+		cout << "#" << t << ' ';
+		cout << a * a << '\n';
+	}
+
+	return 0;
+}
