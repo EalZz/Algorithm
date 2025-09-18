2724182da568d8e090cb0b828bcbaa50cadc9b06
EalZz <114982277+EalZz@users.noreply.github.com>
2025-09-18 18:43:45 +0900

    [Silver III] Title: 파도반 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <memory>
+
+using namespace std;
+int cnt = 0, cnt1 = 1e9;
+//int memo[21][21][21];
+long long memo[101];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    //memset(memo, -1, sizeof(memo));
+    int T = 0; cin >> T;
+    //int N = 0; cin >> N;
+    //int a = 0, b = 0, c = 0; cin >> a >> b >> c;
+    //vector<int> v(T);
+    //vector<int> tmp;
+    //vector<vector<int>> v(T, vector<int>(T));
+    //vector<bool> isVisited(T, false);
+    //vector<vector<int>> matrix(b, vector<int>(a, 0));
+    //vector<vector<bool>> isVisited(T, vector<bool>(T, false));
+
+    for (int i = 0; i < T; i++) {
+        int a; cin >> a;
+        memo[1] = memo[2] = memo[3] = 1;
+        memo[4] = memo[5] = 2;
+
+        for (int i = 6; i <= 100; i++) memo[i] = memo[i - 1] + memo[i - 5];
+
+        cout << memo[a] << '\n';
+    }
+
+    return 0;
+}
