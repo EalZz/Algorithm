532f52ac71b81921c27e1f9d7acbc3f5b665fdd2
EalZz <114982277+EalZz@users.noreply.github.com>
2025-09-23 23:22:16 +0900

    [level 2] Title: 프로세스, Time: 0.02 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<int> priorities, int location) {
+    int answer = 1;
+    vector<int> pri(priorities.size());
+    priority_queue<int> pq;
+    for(int i = 0; i < priorities.size(); i++) {
+        pri[i] = priorities[i];
+        pq.push(priorities[i]);
+    }
+    
+    int idx = 0;
+    while(1) {
+        if(idx > pri.size()) idx = 0;
+        if(pri[idx] == -1) idx++;
+        
+        if(pri[idx] == pq.top()) {
+            if(pri[idx] == priorities[location] && idx == location) return answer;
+            pri[idx] = -1;
+            pq.pop();
+            answer++;
+        }
+        idx++; 
+    }
+    
+    return -1;
+}
