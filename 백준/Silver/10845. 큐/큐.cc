04a27bdf2d8f21486cff1a27f4c502b038465e32
EalZz <114982277+EalZz@users.noreply.github.com>
2025-09-24 18:09:30 +0900

    [Silver IV] Title: 큐, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <memory>
+
+using namespace std;
+int cnt = 0, cnt1 = 1e9;
+//int cnt_1 = 0, cnt0 = 0, cnt1 = 0;
+//int memo[21][21][21];
+//long long memo[101];
+int memo[101][4];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    memset(memo, -1, sizeof(memo));
+    int T = 0; cin >> T;
+    //int N = 0; cin >> N;
+    //int a = 0, b = 0, c = 0; cin >> a >> b >> c;
+    //vector<int> v(6, 0);
+    //vector<vector<int>> v(T, vector<int>(T));
+    //vector<bool> isVisited(T, false);
+    //vector<vector<int>> matrix(b, vector<int>(a, 0));
+    //vector<vector<bool>> isVisited(T, vector<bool>(T, false));
+
+    queue<int> q;
+
+    string cmd = "";
+    while (T--) {
+        cin >> cmd;
+
+        if (cmd == "push") {
+            int a; cin >> a;
+            q.push(a);
+        }
+        else if (cmd == "pop") {
+            if (q.empty()) cout << -1 << '\n';
+            else {
+                cout << q.front() << '\n';
+                q.pop();
+            }
+        }
+        else if (cmd == "size") cout << q.size() << '\n';
+        else if (cmd == "empty") {
+            if (q.empty()) cout << 1 << '\n';
+            else cout << 0 << '\n';
+        }
+        else if (cmd == "front") {
+            if (q.empty()) cout << -1 << '\n';
+            else cout << q.front() << '\n';
+        }
+        else if (cmd == "back") {
+            if (q.empty()) cout << -1 << '\n';
+            else cout << q.back() << '\n';
+        }
+    }
+
+    return 0;
+}
