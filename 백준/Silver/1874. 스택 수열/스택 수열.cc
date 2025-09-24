dca0f395cf96848161c5244301a57d1842407a40
EalZz <114982277+EalZz@users.noreply.github.com>
2025-09-24 22:20:10 +0900

    [Silver II] Title: 스택 수열, Time: 16 ms, Memory: 3024 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <memory>
+
+using namespace std;
+int cnt = 0, cnt1 = 1e9;
+//int cnt_1 = 0, cnt0 = 0, cnt1 = 0;
+//int memo[21][21][21];
+//long long memo[101];
+int memo[101][4];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    memset(memo, -1, sizeof(memo));
+    int T = 0; cin >> T;
+    //int N = 0; cin >> N;
+    //int a = 0, b = 0, c = 0; cin >> a >> b >> c;
+    vector<int> v(T);
+    //vector<vector<int>> v(T, vector<int>(2));
+    //vector<bool> isVisited(T, false);
+    //vector<vector<int>> matrix(b, vector<int>(a, 0));
+    //vector<vector<bool>> isVisited(T, vector<bool>(T, false));
+
+    for (int i = 0; i < T; i++) cin >> v[i];
+
+    stack<int> st;
+    string pp = "";
+    int idx = 0;
+    for (int i = 1; i <= T; i++) {
+        st.push(i);
+        pp.push_back('+');
+
+        while (1) {
+            if (idx >= v.size() || st.empty()) break;
+            if (v[idx] == st.top()) {
+                idx++;
+                st.pop();
+                pp.push_back('-');
+            }
+            else break;
+        }
+    }
+    if (st.empty()) for (auto& c : pp) cout << c << '\n';
+    else cout << "NO";
+
+    return 0;
+}
