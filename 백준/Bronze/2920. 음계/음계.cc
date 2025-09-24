ad2042fb7e4f094eb86c2f23f136d50da08e7ec7
EalZz <114982277+EalZz@users.noreply.github.com>
2025-09-24 13:56:06 +0900

    [Bronze II] Title: 음계, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <memory>
+
+using namespace std;
+int cnt = 0, cnt1 = 1e9;
+//int cnt_1 = 0, cnt0 = 0, cnt1 = 0;
+//int memo[21][21][21];
+//long long memo[101];
+int memo[101][4];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    memset(memo, -1, sizeof(memo));
+    //int T = 0; cin >> T;
+    //int N = 0; cin >> N;
+    //int a = 0, b = 0, c = 0; cin >> a >> b >> c;
+    //vector<int> v(8, 0);
+    //vector<vector<int>> v(T, vector<int>(T));
+    //vector<bool> isVisited(T, false);
+    //vector<vector<int>> matrix(b, vector<int>(a, 0));
+    //vector<vector<bool>> isVisited(T, vector<bool>(T, false));
+    string tmp = "";
+    getline(cin, tmp);
+
+    if (tmp == "1 2 3 4 5 6 7 8") cout << "ascending";
+    else if (tmp == "8 7 6 5 4 3 2 1") cout << "descending";
+    else cout << "mixed";
+
+    return 0;
+}
