111b2274f3eff19ad27c0acad88eb5e8354ffbf5
EalZz <114982277+EalZz@users.noreply.github.com>
2025-09-24 13:35:40 +0900

    [Bronze III] Title: ACM 호텔, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <memory>
+
+using namespace std;
+int cnt = 0, cnt1 = 1e9;
+//int cnt_1 = 0, cnt0 = 0, cnt1 = 0;
+//int memo[21][21][21];
+//long long memo[101];
+int memo[101][4];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    memset(memo, -1, sizeof(memo));
+    int T = 0; cin >> T;
+    //int N = 0; cin >> N;
+    //int a = 0, b = 0, c = 0; cin >> a >> b >> c;
+    //vector<int> v(T);
+    //vector<vector<int>> v(T, vector<int>(T));
+    //vector<bool> isVisited(T, false);
+    //vector<vector<int>> matrix(b, vector<int>(a, 0));
+    //vector<vector<bool>> isVisited(T, vector<bool>(T, false));
+
+    while (T--) {
+        int H, W, g; cin >> H >> W >> g;
+
+        int result = 0;
+        if (g % H != 0) result = (g % H * 100) + (g / H + 1);
+        else result = (H * 100) + (g / H);
+        cout << result << '\n';
+    }
+
+    return 0;
+}
