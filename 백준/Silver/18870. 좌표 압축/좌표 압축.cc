ed35d25d2263a0fa03e13ba282c2c095d78ab985
EalZz <114982277+EalZz@users.noreply.github.com>
2025-09-25 23:13:37 +0900

    [Silver II] Title: 좌표 압축, Time: 436 ms, Memory: 9848 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <memory>
+
+using namespace std;
+int cnt = 0, cnt1 = 1e9;
+//int cnt_1 = 0, cnt0 = 0, cnt1 = 0;
+//int memo[21][21][21];
+long long memo[1001];
+//int memo[101][4];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    //memset(memo, -1, sizeof(memo));
+    int T = 0; cin >> T;
+    //int N = 0; cin >> N;
+    //int a = 0, b = 0, c = 0; cin >> a >> b >> c;
+    vector<int> v(T);
+    //vector<vector<int>> v(T + 1);
+    //vector<bool> isVisited(T + 1, false);
+    //vector<vector<int>> matrix(b, vector<int>(a, 0));
+    //vector<vector<bool>> isVisited(T, vector<bool>(T, false));
+
+    for (int i = 0; i < T; i++) cin >> v[i];
+    vector<int> standard(v.begin(), v.end());
+    sort(standard.begin(), standard.end());
+    standard.erase(unique(standard.begin(), standard.end()), standard.end());
+
+    for (auto& c : v) cout << lower_bound(standard.begin(), standard.end(), c) - standard.begin() << ' ';
+
+    return 0;
+}
