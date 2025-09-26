e986e68a833a670baa2d4536afe6fdef300bab0f
EalZz <114982277+EalZz@users.noreply.github.com>
2025-09-26 17:07:29 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <memory>
+
+using namespace std;
+int cnt = 0, cnt1 = 1e9;
+//int cnt_1 = 0, cnt0 = 0, cnt1 = 0;
+//int memo[21][21][21];
+long long memo[301];
+//int memo[101][4];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    //memset(memo, -1, sizeof(memo));
+    int T = 0; cin >> T;
+    //int N = 0; cin >> N;
+    //int a = 0, b = 0, c = 0; cin >> a >> b >> c;
+    //vector<int> v(T + 1, 0);
+    //vector<vector<int>> v(T, vector<int>(N, 0));
+    //vector<vector<bool>> isVisited(T, vector<bool>(N, false));
+    //vector<vector<int>> matrix(b, vector<int>(a, 0));
+    //vector<vector<bool>> isVisited(T, vector<bool>(T, false));
+
+    while (T--) {
+        int a; cin >> a;
+        map<string, int> clothes;
+
+        for (int i = 0; i < a;i++) {
+            string s, c;
+            cin >> s >> c;
+            if (clothes.find(c) == clothes.end()) clothes[c] = 1;
+            else clothes[c] += 1;
+        }
+
+        cnt = 1;
+        for (auto& c : clothes) cnt *= (c.second + 1);
+        cout << cnt - 1 << '\n';
+    }
+
+    return 0;
+}
