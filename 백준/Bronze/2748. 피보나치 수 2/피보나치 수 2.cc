dfdc3bd2a96f439c5fc7dae000ee9c2137a2c81c
EalZz <114982277+EalZz@users.noreply.github.com>
2025-10-05 20:56:26 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <memory>
+
+using namespace std;
+int cnt = 0, cnt1 = 1e9;
+//int cnt_1 = 0, cnt0 = 0, cnt1 = 0;
+//int memo[21][21][21];
+//long long memo[3];
+//long long memo[101][10];
+long long memo[100001];
+long long MOD = 1000000000;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T = 0; cin >> T;
+    //int N = 0; cin >> N;
+    //int a = 0, b = 0, c = 0; cin >> a >> b >> c;
+    vector<int> v(T + 1, 0);
+    //vector<vector<int>> v(N , vector<int>(2));
+    //vector<bool> isVisited(T + 1, false);
+    //vector<vector<bool>> isVisited(T, vector<bool>(T, false));
+    //vector<vector<int>> matrix(b, vector<int>(a, 0));
+    //vector<vector<bool>> isVisited(T, vector<bool>(T, false));
+
+    memo[0] = 0;
+    memo[1] = memo[2] = 1;
+    for (int i = 3; i <= T; i++) memo[i] = memo[i - 1] + memo[i - 2];
+
+    cout << memo[T];
+
+    return 0;
+}
