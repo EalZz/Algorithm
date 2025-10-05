04a9a3ef2a57fe354940748871b86d0302b051ff
EalZz <114982277+EalZz@users.noreply.github.com>
2025-10-05 20:03:09 +0900

    [Silver I] Title: 포도주 시식, Time: 0 ms, Memory: 2960 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <memory>
+
+using namespace std;
+int cnt = 0, cnt1 = 1e9;
+//int cnt_1 = 0, cnt0 = 0, cnt1 = 0;
+//int memo[21][21][21];
+//long long memo[3];
+//long long memo[101][10];
+long long memo[100001];
+long long MOD = 1000000000;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T = 0; cin >> T;
+    //int N = 0; cin >> N;
+    //int a = 0, b = 0, c = 0; cin >> a >> b >> c;
+    vector<int> v(T + 1, 0);
+    //vector<vector<int>> v(N , vector<int>(2));
+    //vector<bool> isVisited(T + 1, false);
+    //vector<vector<bool>> isVisited(T, vector<bool>(T, false));
+    //vector<vector<int>> matrix(b, vector<int>(a, 0));
+    //vector<vector<bool>> isVisited(T, vector<bool>(T, false));
+
+    for (int i = 1; i <= T; i++) cin >> v[i];
+
+    memo[1] = v[1];
+    memo[2] = memo[1] + v[2];
+
+    for (int i = 3; i <= T; i++) 
+        memo[i] = max(memo[i - 1],
+        max(memo[i - 2] + v[i],
+            memo[i - 3] + v[i - 1] + v[i]));
+
+    cout << memo[T];
+
+    return 0;
+}
