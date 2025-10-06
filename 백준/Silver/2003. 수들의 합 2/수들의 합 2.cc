69c97c506eab6f5bccfc275dc4bb1b50501cfa96
EalZz <114982277+EalZz@users.noreply.github.com>
2025-10-06 22:25:57 +0900

    [Silver IV] Title: 수들의 합 2, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <memory>
+
+using namespace std;
+int cnt = 0, cnt1 = 1e9;
+//int cnt_1 = 0, cnt0 = 0, cnt1 = 0;
+//int memo[21][21][21];
+//long long memo[3];
+//long long memo[101][10];
+//long long memo[100001];
+//long long MOD = 1000000000;
+
+int bfs(int start, vector<vector<int>>& v, vector<bool>& isVisited);
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T = 0; cin >> T;
+    long long N = 0; cin >> N;
+    //int a = 0, b = 0, c = 0; cin >> a >> b >> c;
+    vector<int> v(T, 0);
+    //vector<vector<int>> v(N , vector<int>(2));
+    //vector<bool> isVisited(T + 1, false);
+    //vector<vector<bool>> isVisited(T, vector<bool>(T, false));
+    //vector<vector<int>> matrix(b, vector<int>(a, 0));
+    //vector<vector<bool>> isVisited(T, vector<bool>(T, false));
+
+    for (int i = 0; i < T; i++) cin >> v[i];
+
+    long long sum = 0;
+    int idx = 0;
+    for (int i = 0; i < T; i++) {
+        sum += v[i];
+        while (sum > N) {
+            sum -= v[idx];
+            idx++;
+        }
+        if (sum == N) cnt++;
+    }
+    cout << cnt;
+
+    return 0;
+}
