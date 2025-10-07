a45fd75af597b13a055b21c15026c9dcf7128ead
EalZz <114982277+EalZz@users.noreply.github.com>
2025-10-07 17:36:07 +0900

    [Gold IV] Title: 팰린드롬?, Time: 200 ms, Memory: 2756 KB -BaekjoonHub

---
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <memory>
+
+using namespace std;
+int cnt = 0, cnt1 = 1e9;
+//int cnt_1 = 0, cnt0 = 0, cnt1 = 0;
+//int memo[21][21][21];
+//long long memo[3];
+//long long memo[101][10];
+//long long memo[100001];
+//long long MOD = 1000000000;
+
+bool dfs(int row, int col, vector<vector<int>>& v, vector<vector<bool>>& brow, vector<vector<bool>>& bcol, vector<vector<bool>>& bbox);
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T = 0; cin >> T;
+    //int N = 0; cin >> N;
+    //int a = 0, b = 0, c = 0; cin >> a >> b >> c;
+    vector<int> v(T + 1, 0);
+    //vector<vector<int>> v(N , vector<int>(2));
+    //vector<bool> isVisited(T + 1, false);
+    //vector<vector<bool>> isVisited(T, vector<bool>(T, false));
+    //vector<vector<int>> matrix(b, vector<int>(a, 0));
+    //vector<vector<bool>> isVisited(T, vector<bool>(T, false));
+    vector<vector<bool>> isPelin(2001, vector<bool>(2001, false));
+    for (int i = 1; i <= T; i++) {
+        cin >> v[i];
+        isPelin[i][i] = true;
+    }
+    for (int i = 1; i <= T - 1; i++) {
+        if (v[i] == v[i + 1]) isPelin[i][i + 1] = true;
+    }
+    for (int i = 2; i <= T; i++) {
+        for (int j = 1; j < i; j++) {
+            if (isPelin[j + 1][i - 1] && v[j] == v[i]) isPelin[j][i] = true;
+        }
+    }
+
+    int N; cin >> N;
+    while (N--) {
+        int s, e; 
+        cin >> s >> e;
+        if (isPelin[s][e]) cout << 1 << '\n';
+        else cout << 0 << '\n';
+    }
+
+    return 0;
+}
