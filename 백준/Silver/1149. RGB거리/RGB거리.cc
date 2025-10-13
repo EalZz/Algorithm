8f7355f794ca8d039f93e1e00df9669f391295d4
EalZz <114982277+EalZz@users.noreply.github.com>
2025-10-13 12:19:41 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2176 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -13,51 +13,59 @@
 
 using namespace std;
 int cnt = 0, cnt1 = 1e9;
+//int cnt_1 = 0, cnt0 = 0, cnt1 = 0;
 //int memo[21][21][21];
-//long long memo[101];
-int memo[1001][4];
+//long long memo[3];
+int memo[1001][3];
+//long long memo[100001];
+//long long MOD = 1000000000;
 
-int dfs(int level, int before, vector<vector<int>>& v);
+int dfs(int idx, int pre, int first, vector<vector<int>>& v);
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    memset(memo, -1, sizeof(memo));
-    int T = 0; cin >> T;
-    //int N = 0; cin >> N;
+    int N = 0; cin >> N;
+    //int M = 0; cin >> M;
     //int a = 0, b = 0, c = 0; cin >> a >> b >> c;
-    //vector<int> v(T);
-    //vector<int> tmp;
-    vector<vector<int>> v(T, vector<int>(4, -1));
-    //vector<bool> isVisited(T, false);
+    //vector<long long> v(T + 1, 0);
+    vector<vector<int>> v(N, vector<int>(3, -1));
+    //vector<bool> isVisited(a + 1, false);
+    //vector<vector<bool>> isVisited(T, vector<bool>(T, false));
     //vector<vector<int>> matrix(b, vector<int>(a, 0));
     //vector<vector<bool>> isVisited(T, vector<bool>(T, false));
 
-    for (int row = 0; row < T; row++) {
-        for (int col = 1; col <= 3; col++) cin >> v[row][col];
+    for (int row = 0; row < N; row++) {
+        for (int col = 0; col < 3; col++) cin >> v[row][col];
     }
 
-    cout << dfs(0, 0, v);
+    int result = 1e9;
+    for (int i = 0; i < 3; i++) {
+        memset(memo, -1, sizeof(memo));
+        memo[0][i] = v[0][i];
+        result = min(result, dfs(1, i, i, v) + v[0][i]);
+    }
+
+    cout << result;
 
     return 0;
 }
 
-int dfs(int level, int before, vector<vector<int>>& v) {
-    if (memo[level][before] != -1) return memo[level][before];
+int dfs(int level, int pre, int first, vector<vector<int>>& v) {
+    if (level == v.size()) {
+        //if (pre == first) return 1e9;
+        return 0;
+    }
 
-    if (level == v.size()) return 0;
+    if (memo[level][pre] != -1) return memo[level][pre];
 
     int sum = 1e9;
-    if (before == 0) {
-        for (int i = 1; i <= 3; i++) sum = min(sum, v[level][i] + dfs(level + 1, i, v));
+    for (int i = 0; i < 3; i++) {
+        if (i == pre) continue;
+        sum = min(sum, v[level][i] + dfs(level + 1, i, first, v));
     }
-    else {
-        for (int i = 1; i <= 3; i++) {
-            if (i == before) continue;
-            sum = min(sum, v[level][i] + dfs(level + 1, i, v));
-        }
-    }
-    memo[level][before] = sum;
-    return memo[level][before];
+
+    memo[level][pre] = sum;
+    return memo[level][pre];
 }
