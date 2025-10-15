659d118236eb267ef9bfddef35f646a8d5361e26
EalZz <114982277+EalZz@users.noreply.github.com>
2025-10-15 21:21:05 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - String, Time: 5 ms, Memory: 5,832 KB -BaekjoonHub

---
+++ SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <memory>
+
+using namespace std;
+//int cnt = 0, cnt1 = 0;
+//int memo[21][21][21];
+//int memo[10000001];
+
+int main() {
+	int T = 10;// cin >> T;
+
+	for (int t = 1; t <= T; t++) {
+		int test, result = 0; cin >> test;
+
+		string str, token;
+		cin >> token >> str;
+
+		for (int i = 0; i < str.size() - token.size() + 1; i++) {
+			if (token[0] != str[i]) continue;
+			if (token == str.substr(i, token.size())) result++;
+		}
+
+		cout << "#" << t << ' ' << result << '\n';
+	}
+
+	return 0;
+}
