a0597822889076d036114d4300822e469095e473
EalZz <114982277+EalZz@users.noreply.github.com>
2025-10-29 17:14:10 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문2, Time: 5 ms, Memory: 5,844 KB -BaekjoonHub

---
+++ SWEA/D3/1229. ［S／W 문제해결 기본］ 8일차 － 암호문2/［S／W 문제해결 기본］ 8일차 － 암호문2.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <memory>
+
+using namespace std;
+
+int main() {
+	int T = 10; //cin >> T;
+
+	for (int t = 1; t <= T; t++) {
+		//int tc; cin >> tc;
+
+		vector<int> cord;
+		int N; cin >> N;
+		for (int i = 0; i < N; i++) {
+			int tmp; cin >> tmp;
+			cord.push_back(tmp);
+		}
+
+		cin >> N;
+		while (N--) {
+			char c; cin >> c;
+			vector<int> tmp;
+			switch (c) {
+				int x, y;
+			case 'I':
+				cin >> x >> y;
+				for (int i = 0; i < y; i++) {
+					int s; cin >> s;
+					tmp.push_back(s);
+				}
+				cord.insert(cord.begin() + x, tmp.begin(), tmp.end());
+				break;
+			case 'D':
+				cin >> x >> y;
+				cord.erase(cord.begin() + x, cord.begin() + x + y);
+				break;
+			}
+		}
+
+		cout << "#" << t << ' ';
+		for (int i = 0; i < 10; i++) cout << cord[i] << ' ';
+		cout << '\n';
+	}
+
+	return 0;
+}
