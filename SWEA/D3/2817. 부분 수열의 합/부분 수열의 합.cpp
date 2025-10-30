3357edc3e80bf843adba735a48486ad0a29aa0af
EalZz <114982277+EalZz@users.noreply.github.com>
2025-10-31 01:51:08 +0900

    [D3] Title: 부분 수열의 합, Time: 4 ms, Memory: 396 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.cpp
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.cpp
@@ -12,37 +12,24 @@
 
 using namespace std;
 
-void dfs(int K, int idx, int sum, vector<int>& v);
-int cnt = 0;
-
 int main() {
-	int T = 0; cin >> T;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-	for (int t = 1; t <= T; t++) {
-		//int tc; cin >> tc;
-		int N, K; cin >> N >> K;
-		vector<int> v(N);
-		for (int i = 0; i < N; i++) cin >> v[i];
+    int T; cin >> T;
+    for (int t = 1; t <= T; t++) {
+        int N, K; cin >> N >> K;
 
-		cnt = 0;
-		dfs(K, 0, 0, v);
-		if (K == 0) cnt--;
+        int dp[1001] = { 0, };
+        dp[0] = 1;
 
-		cout << "#" << t << ' ' << cnt << ' ';
-		cout << '\n';
-	}
+        while (N--) {
+            int num; cin >> num;
 
-	return 0;
-}
+            for (int i = K; i >= num; i--) dp[i] += dp[i - num];
+        }
 
-void dfs(int K, int idx, int sum, vector<int>& v) {
-	if (idx == v.size()) return;
-
-	dfs(K, idx + 1, sum, v);
-	sum += v[idx];
-	dfs(K, idx + 1, sum, v);
-	
-	if (sum == K) cnt++;
-
-	return;
+        cout << "#" << t << ' ' << dp[K] << '\n';
+    }
+    return 0;
 }
