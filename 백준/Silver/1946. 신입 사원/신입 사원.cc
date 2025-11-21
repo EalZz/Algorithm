9a77af6e21dc979c54434acf4aa7fd02aee5d396
EalZz <114982277+EalZz@users.noreply.github.com>
2025-11-21 12:42:03 +0900

    [Silver I] Title: 신입 사원, Time: 432 ms, Memory: 2920 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.cc
+++ 백준/Silver/1946. 신입 사원/신입 사원.cc
@@ -14,32 +14,26 @@
 
 using namespace std;
 
-int dy[4] = { 1, -1, 0, 0 };
-int dx[4] = { 0, 0, 1, -1 };
-
-void bfs(int row, int col, vector<vector<int>>& v, vector<vector<bool>>& isVisited);
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
 
     int tc; cin >> tc;
-
     while (tc--) {
         int N; cin >> N;
-
         vector<pair<int, int>> v(N);
         for (int i = 0; i < N; i++) cin >> v[i].first >> v[i].second;
         sort(v.begin(), v.end());
 
-        int cnt = 0, maxs = v[0].second;
+        int ans = 0, maxScore = v[0].second;
         for (int i = 0; i < N; i++) {
-            if (v[i].second > maxs) continue;
-            cnt++;
-            maxs = v[i].second;
+            if (v[i].second > maxScore) continue;
+            ans++;
+            maxScore = v[i].second;
         }
-        cout << cnt << '\n';
+
+        cout << ans << '\n';
     }
 
     return 0;
