68111196807a1413d90abd4d4427908db2ff8538
EalZz <114982277+EalZz@users.noreply.github.com>
2025-12-12 11:20:04 +0900

    [Silver III] Title: 이전 순열, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Silver/10973. 이전 순열/이전 순열.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <memory>
+#include <list>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int N; cin >> N;
+    vector<int> v(N);
+    for (int i = 0; i < N; i++) cin >> v[i];
+    if (prev_permutation(v.begin(), v.end())) {
+        for (auto& i : v) cout << i << ' ';
+    }
+    else cout << -1;
+    
+    return 0;
+}
