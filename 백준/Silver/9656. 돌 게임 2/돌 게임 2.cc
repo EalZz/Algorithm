e1703240e727e3b5322c8cf9c78fdc4cfdb29a4f
EalZz <114982277+EalZz@users.noreply.github.com>
2025-12-19 18:22:28 +0900

    [Silver V] Title: 돌 게임 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <memory>
+#include <list>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int N; cin >> N;
+    string s;
+    s = (N % 2) ? "CY" : "SK";
+
+    cout << s;
+
+    return 0;
+}
