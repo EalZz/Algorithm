034463e8da4e87f056695f13847cb07f3ea91f8b
EalZz <114982277+EalZz@users.noreply.github.com>
2026-01-03 18:47:26 +0900

    [Silver IV] Title: 30, Time: 0 ms, Memory: 2300 KB -BaekjoonHub

---
+++ 백준/Silver/10610. 30/30.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <memory>
+#include <list>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    string s; cin >> s;
+    sort(s.begin(), s.end(), greater<>());
+
+    if (s[s.length() - 1] != '0') cout << -1;
+    else {
+        long long sum = 0;
+        for (auto o : s) sum += o - '0';
+        if (sum % 3 == 0) cout << s;
+        else cout << -1;
+    }
+
+    return 0;
+}
