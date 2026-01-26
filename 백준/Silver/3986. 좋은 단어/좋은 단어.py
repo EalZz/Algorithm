491c3443c5798ffad54a0188a8bcf81bfa5183b0
EalZz <114982277+EalZz@users.noreply.github.com>
2026-01-26 09:10:09 +0900

    [Silver IV] Title: 좋은 단어, Time: 144 ms, Memory: 130360 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.py
@@ -0,0 +1,24 @@
+import sys
+from collections import deque
+import math
+
+#input = sys.stdin.readline 
+
+def solve():
+    n = int(input())
+    ans = 0
+
+    for _ in range(n):
+        stack = []
+        _list = list(input())
+        for i in _list:
+            if not len(stack): stack.append(i)
+            elif stack[-1] == i: stack.pop(-1)
+            else: stack.append(i)
+
+        if not len(stack): ans += 1 
+
+    print(ans)
+
+if __name__ == "__main__":
+    solve()
